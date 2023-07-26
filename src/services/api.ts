df7b4baae12c5760e327ced26860f7f3f36bad78
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-07-26 14:11:11 +0000

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { CreateAxiosDefaults, AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios'
 
 export class API {
-    private constructor() { }
+    private constructor() {}
 
     public static createApi(config?: CreateAxiosDefaults<any>) {
         const api = new ApiCore(config)
@@ -18,25 +18,37 @@ export class ApiCore {
     }
 
     public async get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.get<T, R, D>(url, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .get<T, R, D>(url, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async post<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.post<T, R, D>(url, data, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .post<T, R, D>(url, data, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async delete<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.delete<T, R, D>(url, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .delete<T, R, D>(url, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async put<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.put<T, R, D>(url, data, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .put<T, R, D>(url, data, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
@@ -48,31 +60,58 @@ export class ApiCore {
     }
 
     public async request<T = any, R = AxiosResponse<T>, D = any>(config: AxiosRequestConfig<D>) {
-        const response = await this.api.request<T, R, D>(config).then(res => res).catch(err => err)
+        const response = await this.api
+            .request<T, R, D>(config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async head<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.head<T, R, D>(url, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .head<T, R, D>(url, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async postForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.postForm<T, R, D>(url, data, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .postForm<T, R, D>(url, data, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async putForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.putForm<T, R, D>(url, data, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .putForm<T, R, D>(url, data, config)
+            .then(res => res)
+            .catch(err => err)
 
         return response
     }
 
     public async patchForm<T = any, R = AxiosResponse<T>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
-        const response = await this.api.patchForm<T, R, D>(url, data, config).then(res => res).catch(err => err)
+        const response = await this.api
+            .patchForm<T, R, D>(url, data, config)
+            .then(res => res)
+            .catch(err => err)
+
+        return response
+    }
+
+    public async options<T = any, R = AxiosResponse<T, any>, D = any>(url: string, config?: AxiosRequestConfig<D>) {
+        const response = await this.api.options<T, R>(url, config)
+
+        return response
+    }
+
+    public async patch<T = any, R = AxiosResponse<T, any>, D = any>(url: string, data?: D, config?: AxiosRequestConfig<D>) {
+        const response = await this.api.patch<T, R>(url, data, config)
 
         return response
     }
